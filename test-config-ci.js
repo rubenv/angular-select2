3ecb46c3738bfd79228a3ceb3941e450573e7bf2
Ruben Vermeersch <ruben@rocketeer.be>
2014-04-15 14:55:22 +0200

    Fix another configuration mistake.

--- test-config-ci.js
+++ test-config-ci.js
@@ -1,3 +1,3 @@
-var config = require('./test-config');
+var config = require('./test-config').config;
 config.multiCapabilities = require('open-sauce-browsers')('angular-select2');
 exports.config = config;
