823739adbff3cb842d5c4c27665580f90e1e9f4d
Ruben Vermeersch <ruben@rocketeer.be>
2014-08-13 15:25:31 +0200

    Add test for clearing.

---
+++ test/clear.js
@@ -0,0 +1,51 @@
+describe('Clear', function () {
+    var TestPage = function () {
+        this.textInput = element(by.id('simpleText'));
+        this.select = element(by.id('s2id_simpleSelect2'));
+        this.chosen = element(by.css('.select2-chosen'));
+
+        this.get = function () {
+            browser.get('http://localhost:9000/test/fixtures/clear.html');
+        };
+
+        this.getInputValue = function () {
+            return this.textInput.getAttribute('value');
+        };
+
+        this.setInputValue = function (value) {
+            this.textInput.clear();
+            this.textInput.sendKeys(value);
+        };
+
+        this.getChosenLabel = function () {
+            return this.chosen.getText();
+        };
+
+        this.selectValue = function (index) {
+            this.select.click();
+            element(by.css('.select2-result:nth-child(' + index + ')')).click();
+        };
+
+        this.clearValue = function () {
+            element(by.css('.select2-search-choice-close')).click();
+        };
+
+        this.get();
+    };
+
+    var page;
+    beforeEach(function () {
+        page = new TestPage();
+    });
+
+    it('Should allow clear', function () {
+        page.selectValue(2);
+        expect(page.getInputValue()).toEqual('2');
+        expect(page.getChosenLabel()).toEqual('Two');
+
+        page.clearValue();
+        expect(page.getInputValue()).toEqual('');
+        expect(page.getChosenLabel()).toEqual('None');
+    });
+});
+
