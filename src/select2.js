b3df991b154de17b55fb721f41f3a626a90469f0
XciD <adrien@xcid.fr>
2015-04-22 16:51:47 +0200

    Update select2.js

--- src/select2.js
+++ src/select2.js
@@ -248,7 +248,7 @@ angular.module("rt.select2", [])
                     var select2Element = element.select2(opts);
                     var select2Focusser = select2Element.select2("container").find(".select2-focusser");
                     if(element.attr("required") && select2Focusser){
-                        $(select2Focusser).attr('required', '');
+                        select2Focusser.attr("required", "");
                     }
                     element.on("change", function (e) {
                         scope.$apply(function () {
@@ -268,7 +268,7 @@ angular.module("rt.select2", [])
                             }
                             
                             if(element.attr("required") && select2Focusser){
-                                $(select2Focusser).removeAttr('required');
+                                select2Focusser.removeAttr("required");
                             }
 
                             controller.$render();
